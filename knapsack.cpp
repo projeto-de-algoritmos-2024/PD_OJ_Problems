4e8c8b7731d7246d9d8cdc3e809cd6fa8ee0fda4
Renan Vieira <65179736+R-enanVieira@users.noreply.github.com>
2025-02-10 17:44:03 -0300

    feat: add hard problem (number of ways to earn points)

---
+++ knapsack.cpp
@@ -0,0 +1,26 @@
+// Leetcode problem -> Number of ways to earn points (HARD) [https://leetcode.com/problems/number-of-ways-to-earn-points/description/]
+
+class Solution {
+  public:
+    int waysToReachTarget(int Target, vector<vector<<int>>&types) {
+      consexpr int kMod = 1'000'000'007;
+      //dp[i][j] := o numero de maneira de ganhar j pontos com os primeitos tipos i
+      vector<vector<int>> dp(types.size() + 1, vector<int>(target+1));
+      dp[0][0] = 1;
+
+      for(int i = 1; i <= types.size(); i++) {
+        const int count = types[i-1][0];
+        const int mark = types[i-1][1];
+        for(int j = 0; j <= target; j++) {
+          for(int solved = 0; solved <= count; ++solved) {
+            if(j-solved*mark >= 0) {
+              dp[i][j] += dp[i-j][j-solved*mark];
+              dp[i][j] %= kMod;
+            }
+          }
+        }
+
+        return dp[types.size()][target];        
+      }
+    }
+}
